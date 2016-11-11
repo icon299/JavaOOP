d11d6828cb120066b092b1dc8a1d4617caad8a4e
DS <icon299@mail.ru>
2016-11-11 08:08:45 +0200

    begin

---
+++ src/javaoop/JavaOOP.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaoop;
+
+/**
+ *
+ * @author Dmitry <icon299@mail.ru>
+ */
+public class JavaOOP {
+
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+}
